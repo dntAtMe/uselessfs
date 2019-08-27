1b132650671f63ead3973a66786e25cd99593320
dntAtMe <dntatm3@gmail.com>
2019-08-27 23:57:38 +0200

    loggin and readdir template

--- main.c
+++ main.c
@@ -10,20 +10,32 @@
 #include <sys/stat.h>
 
 static int do_getattr( const char *path, struct stat *st ) {
+	printf("[getattr] Running\n");
+	printf("[getattr] Requested  path:%s\n", path);
+	
 	st->st_uid = getuid();	
 	st->st_gid = getgid();
 	st->st_atime = time( NULL );
 	st->st_mtime = time( NULL );
 	
 	if ( strcmp(path, "/") == 0 ) {
+		printf("[getattr] Directory found");
 		st->st_mode = __S_IFDIR | 0755;
 		st->st_nlink = 2;
 	} else {
+		printf("[getattr] File found");
 		st->st_mode = __S_IFREG | 0644;
 		st->st_nlink = 1;
 		st->st_size = 1024;	
 	}
+	
+	printf("[getattr] End\n");
 	return 0;
+}
+
+static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct file_fuse_info *fi) {	
+	printf("[readdir] Running\n");
+	
 }
 
 
