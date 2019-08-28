edbb47b4d250082de0f5cf5682a7167168f4a85b
dntAtMe <dntatm3@gmail.com>
2019-08-28 16:29:22 -0400

    working fs, readme and gitignore

---
+++ README.md
@@ -0,0 +1,11 @@
+# Useless Filesystem
+
+
+Useless filesystem is just one of many useless filesystems, i guess.
+
+### Mounting
+```
+$ gcc main.c -o uselessfs -D_FILE_OFFSET_BITS=64 -I/usr/include/fuse -lfuse -pthread
+./uselessfs [-d] [mountdir]
+```
+-d debugging option
--- main.c
+++ main.c
@@ -17,7 +17,7 @@ static int do_getattr( const char *path, struct stat *st ) {
 	st->st_gid = getgid();
 	st->st_atime = time( NULL );
 	st->st_mtime = time( NULL );
-	
+
 	if ( strcmp(path, "/") == 0 ) {
 		printf("[getattr] Directory found");
 		st->st_mode = __S_IFDIR | 0755;
@@ -36,7 +36,46 @@ static int do_getattr( const char *path, struct stat *st ) {
 static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct file_fuse_info *fi) {	
 	printf("[readdir] Running\n");
 	
+	filler(buffer, ".", NULL, 0);
+	filler(buffer, "..", NULL, 0);
+
+	if(strcmp(path, "/") == 0) {
+		filler(buffer, "file54", NULL, 0);
+		filler(buffer, "file49", NULL, 0);
+	}
+
+	return 0;
 }
+
+static int do_read(const char *path, char *buffer, size_t size, off_t offset, struct file_fuse_info *fi) {
+	printf("[read] Running\n");
+	
+	char file54text[] = "file54";
+	char file49text[] = "\n\nfile49";	
+
+	if( strcmp(path, "/file53") == 0 ) {
+		memcpy(buffer, file54text + offset, size);
+		return strlen(file54text) - offset;
+	} else if( strcmp(path, "/file49") == 0 ) {
+		memcpy(buffer, file49text + offset, size);
+		return strlen(file49text) - offset;
+	} else 
+		return -1;	
+}
+
+
+
+static struct fuse_operations operations = {
+	.getattr = do_getattr,
+	.readdir = do_readdir,
+	.read = do_read,
+};
+
+int main(int argc, char* argv[]) {
+	return fuse_main(argc, argv, &operations, NULL);
+}
+
+
 
 
 
