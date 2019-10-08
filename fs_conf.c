5537a937d17395ee4b7817f658f1be25ffecd56a
dntAtMe <dntatm3@gmail.com>
2019-10-08 18:27:28 +0200

    playing around with config

--- fs_conf.c
+++ fs_conf.c
@@ -1,6 +1,11 @@
 #include <sys/stat.h>
+#include <string.h>
 #include <time.h>
-
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 typedef struct {
     char *path;
@@ -13,3 +18,20 @@ struct uselessfs_config {
     char *mountpoint;
     replica_t *replicas;
 };
+
+int main() {
+    int fd = open("config/template.cfg", O_RDONLY);
+    int code;
+    char buffer[20];
+    memset(buffer, 0x00, 20);
+    while (1){
+        code = read(fd, &buffer, 1); 
+	if (!code)
+		break;
+	printf("code: %d ", code);
+	printf("%s\n", buffer);
+    	sleep(1);
+    }
+
+    return 0;
+}
